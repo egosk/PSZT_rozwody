740a9bb056575f23e8743bf1b06465fee5f86de1
emigos <emilia.gosk@syncron.com>
2020-06-09 18:18:19 +0200

    Code clean-up, comments, building models and validating them using k-fold

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+# program prepared for PSZT course
+# project 2 - machine learning
+# id3 alghoritm + k-fold cross validation
+# author: Emilia Gosk
+
 import pandas as pd
 import numpy as np
 from numpy import log
 from sklearn.model_selection import KFold
 
+
 # small value used to prevent division by 0 and log(0)
 epsilon = np.finfo(float).eps
 
-dane = []
 
 # function used to import the data set and
 # process it into data frame
@@ -22,23 +27,16 @@ def data_import():
     temp_data = zip(raw_data[0], attributes) # creates tuple (column_names, attributes)
     dictionary_dataset = dict(temp_data) # creates dictionary: Atr1: val_1, val_2.. , Atr2: val_1, val_2 ....
     divorces_df = pd.DataFrame(dictionary_dataset, columns=raw_data[0]) # creates table
-    # print(divorces_df)
-    # print(klasy)
 
     return divorces_df
 
-dane_test = data_import()
-#print(dane_test)
 
+# function returns 2 sets of data
+# training set and test set
+# splitting it into set in ratio (k-1):1
 def k_cross_valid_data_sets(my_data_frame, k):
     kfold = KFold(k, True, 1)
     kf = kfold.split(my_data_frame)
-
-    # i = 0
-    # for train, test in kf:
-    #     print(i)
-    #     print('train: %s, test: %s' % (train, test))
-    #     i += 1
 
     training_set_indexes = []
     test_set_indexes = []
@@ -58,43 +56,17 @@ def k_cross_valid_data_sets(my_data_frame, k):
         train_dataframe = pd.DataFrame(my_data_frame.loc[training_set_indexes[i]])
         train_dataframes.append(train_dataframe)
 
-
-    # print(test_dataframes[0])
-    # print(train_dataframes[0])
     return train_dataframes, test_dataframes
 
-train, test = k_cross_valid_data_sets(dane_test, 10)
 
-
-# print(training_set_indexes[0][2])
-# print(test_set_indexes[0])
-
-
-
-# test_data =  dane_test.loc[[1]]
-# print(test_data)
-
-
-
-
-#result = kfold.split(dane_test)
-
-#print(result)
-
-
-# for train_index, test_index in kfold.split(dane_test):
-#     print('TRAIN:', train_index, 'TEST:', test_index)
-#     X_train, X_test = X[train_index], X[test_index]
-#     y_train, y_test = y[train_index], y[test_index]
-
+# function computes entropy of data set splited into subsets
 def entropy(my_data_frame):
     entropy = 0
 
     classes = my_data_frame.keys()[-1] # last column in data set contains classes
     possible_values = my_data_frame[classes].unique() # here: 1 - divorced, 0 - married
-    #print(possible_values)
 
-    #podrecznik str 76:
+    # coursebook p. 76:
     #I(S) = - sum(fc(S) * ln fc(S))
     #I(S) - entropy of whole data set
     #fc(S) = frequency of appearing class c in set S
@@ -105,18 +77,15 @@ def entropy(my_data_frame):
     return entropy
 
 
-# print(entropy(dane_test))
-
 # function computes entropy for provided attribute
-
-def attribue_entropy (my_data_frame, attr):
+def attribute_entropy(my_data_frame, attr):
     attr_entropy = 0
 
     classes = my_data_frame.keys()[-1]   # last column in data set contains classes
     class_variables = my_data_frame[classes].unique()  # here: 1 - divorced, 0 - married
     variables = my_data_frame[attr].unique()    # unique different values in that attribute
 
-    # podr str 76:
+    # coursebook p. 76:
     # equation for entropy of subset:
     # Inf(D,S) = sum { (|Sj|/|S|) * I(Sj) }
     # |S| - number of elements in set S
@@ -136,33 +105,22 @@ def attribue_entropy (my_data_frame, attr):
 
     return abs_attr_entropy
 
-# for key in dane_test.keys():
-#     print(key, attribue_entropy(dane_test, key))
 
-#print(attribue_entropy(dane_test, 'Atr18'))
-
-
-# function compytes information gain and finds max value
-# podr str 77: InfGain(D,S) = I(S) - Inf(D,S)
+# function computes information gain and finds max value
+# coursebook p. 77: InfGain(D,S) = I(S) - Inf(D,S)
 def max_inf_gain(my_data_frame):
     inf_gain = []
 
     for key in my_data_frame.keys()[:-1]:
-        inf_gain.append(entropy(my_data_frame)-attribue_entropy(my_data_frame,key))
+        inf_gain.append(entropy(my_data_frame)-attribute_entropy(my_data_frame,key))
     return my_data_frame.keys()[:-1][np.argmax(inf_gain)]
 
-# print(entropy(dane_test)-attribue_entropy(dane_test,'Atr18'))
-# print(max_inf_gain(dane_test))
 
 # function returns subtable
 # where attribute attr has value attr_value
-
 def get_subtable(my_data_frame, attr, attr_value):
     return my_data_frame[my_data_frame[attr] == attr_value].reset_index(drop=True)
 
-
-#st = get_subtable(dane_test,'Atr1', '1')
-#print(st)
 
 # function where we build tree
 def id3_tree(my_data_frame, tree=None):
@@ -196,8 +154,79 @@ def id3_tree(my_data_frame, tree=None):
 
     return tree
 
-tr = id3_tree(dane_test)
-# print(tr)
 
-tr1 = id3_tree(train[0])
-print((tr1))
+# function uses tree to predict
+# which class the data_set belongs to
+# returns 1 - divorce
+# returns 0 - married
+def predict(tree, testing_series):
+    c = tree
+    for k, v in testing_series.iteritems():
+        try:
+            c = c[k][v]
+        except KeyError:
+            continue
+        if isinstance(c, dict):  # is dictionary
+            pass
+        else:    # is leaf
+            return c
+
+
+# function calculates  loss for data set
+# coursebook p.62:
+# q(d) = 0 when d=y
+# q(d) = 1 when d!=y
+# q = sum q(d)
+def calculate_loss(tr, tst_set):
+    q = 0
+    for i in range (0, len(tst_set)):
+        if predict(tr, tst_set.iloc[i]) != tst_set.iloc[i][-1]:
+            q += 1
+
+    q = q / len(tst_set)
+    return q
+
+
+# function calulcates average loss for model
+# coursebook p.81
+# q_mod = (1/k)* sum q(d)
+def calculate_model_loss(train, test, k):
+    q_mod = 0
+    for j in range (0, k):
+        q_mod += calculate_loss(id3_tree(train[j]), test[j])
+    q_mod = q_mod / k
+    return q_mod
+
+
+def main():
+    # model built on whole dataset
+    print('Model built on whole dataset: ')
+    dane_test = data_import()
+    tree_full_dataset = id3_tree(dane_test)
+    print(tree_full_dataset)
+
+    # models built on data splited in 3:1 ratio
+    # for ttraining and testing data set
+    # value of loss per model printed
+    print('Models and loss per model for k=4: ')
+    train, test = k_cross_valid_data_sets(dane_test, 4)
+    for i in range(0, 4):
+        tree_4 = id3_tree(train[i])
+        print(tree_4)
+        print('loss:', calculate_loss(tree_4, test[i]))
+
+    # sorted list of pairs
+    # (attribute entropy, attribute name)
+    print('Sorted list of entropy per attribute: ')
+    list_key_entr = []
+    for key in dane_test.keys()[:-1]:
+        key_entr = [attribute_entropy(dane_test, key), key]
+        list_key_entr.append(key_entr)
+    list_key_entr.sort()
+    print(list_key_entr)
+
+
+main()
+
+
+
